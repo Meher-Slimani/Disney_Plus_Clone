b3327a72e2ddb35f943c9f779c154b520556d0e6
Meher Slimani <meher.slimeni@gmail.com>
2021-05-06 00:33:36 +0100

    Add Detail page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/detail" component={Detail} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
